0cf5cf1a9b2baa664f0b3b5a16197a72b3e2dbf6
Hyungju Moon <splex7@users.noreply.github.com>
2020-11-06 14:29:53 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const assert = require("assert");
+const uri = `mongodb+srv://${process.env.MONGOUSER}:${process.env.MONGOPASS}@${process.env.MONGOADDRESS}?retryWrites=true&w=majority`;
+const MongoClient = require("mongodb").MongoClient;
+
+const app = express();
+app.all("/*", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+let dataResult = ["Not loaded yet"];
+async function run(limit) {
+  await MongoClient.connect(
+    uri,
+    {
+      useUnifiedTopology: true,
+      useNewUrlParser: true
+    },
+    //callback of mongoclient connect function
+    async function (err, client) {
+      assert.equal(null, err);
+      console.log("Connected successfully to server");
+      const db = client.db("dream");
+      const collection = db.collection("crawls");
+      const query = { createdAt: { $exists: true } };
+      doSomething(db, collection, query, limit, (err, results) => {
+        client.close();
+        if (err) console.error(err);
+        console.log(results.length);
+        dataResult = results;
+      });
+    }
+  );
+}
+const doSomething = (db, collection, query, limit, callback) => {
+  collection.find(query).limit(limit).toArray(callback);
+};
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.get("/movies", (req, res) => {
+  // let limit = req.query.limit ? parseInt(req.query.limit) : 100;
+  run(100)
+    .then(() => res.json(dataResult))
+    .catch(console.dir);
+});
+
+app.listen(3030, () => {
+  console.log("listening 3030");
+});
